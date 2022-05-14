dcf4d46eb4a0bd286b9a14a1a20983cf8a2a29c7
kklczk <koloczek@student.agh.edu.pl>
2022-05-14 12:48:00 +0200

    Add finding n_pages for cabriolets

--- soup/otomoto_bs.py
+++ soup/otomoto_bs.py
@@ -1,2 +1,12 @@
 from urllib import request
 from bs4 import BeautifulSoup as BS
+import pandas as pd
+
+limit = True
+
+url = 'https://www.otomoto.pl/osobowe/seg-cabrio' 
+html = request.urlopen(url)
+bs = BS(html.read(), 'html.parser')
+
+n_pages = bs.find_all('li', {"data-testid":"pagination-list-item"})[-1].span.text
+print(n_pages)
